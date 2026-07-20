2f94e32f9e4361a231bc0f9776ae6465a6594e79
brandon0638 <brandonandria06@gmail.com>
2026-07-20 11:55:49 +0200

    parametrage route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,28 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
+namespace Config;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index');
+$routes = Services::routes();
+
+// Page d'accueil
+$routes->get('/', 'AuthController::index');
+
+$routes->get('client/login', 'AuthController::clientLoginForm');
+$routes->post('client/login', 'AuthController::clientLogin');
+$routes->get('client/dashboard', 'Client\DashboardController::index');
+
+
+$routes->post('client/depot', 'Client\OperationController::depot');
+$routes->post('client/retrait', 'Client\OperationController::retrait');
+$routes->post('client/transfert', 'Client\OperationController::transfert');
+
+
+$routes->post('client/calculer-frais', 'Client\OperationController::calculerFrais');
+
+
+$routes->get('client/history', 'Client\HistoryController::index');
+
+
+$routes->get('operator/dashboard', 'Operator\DashboardController::index');
+
+$routes->get('logout', 'AuthController::logout');
